8d0593ba7d10afc6a85aadceacc1f55e308ef6f9
Milana <94888u@gmail.com>
2023-08-11 16:25:50 +0200

    add simple enemy

--- src/arena/arena.gd
+++ src/arena/arena.gd
@@ -2,5 +2,8 @@ extends Node2D
 
 
 func _ready() -> void:
-#	$Player.play("idle")
+	$AnimationPlayer.play("fight")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	$AnimationPlayer.play("fight")
