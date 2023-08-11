8a9163cf281d4fa9798b6802ab4481e73d73eb86
Milana <94888u@gmail.com>
2023-08-11 17:16:18 +0200

    fill enemy animations

--- src/arena/arena.gd
+++ src/arena/arena.gd
@@ -5,5 +5,6 @@ func _ready() -> void:
 	$AnimationPlayer.play("fight")
 
 
+# Animation loop
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	$AnimationPlayer.play("fight")
+	$AnimationPlayer.play(anim_name)
