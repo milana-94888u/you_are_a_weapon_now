414dbbd195a715c14c063ee7d1349b523d41c981
Milana <94888u@gmail.com>
2023-08-13 19:58:47 +0200

    add minigame

--- src/arena/arena.gd
+++ src/arena/arena.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+var minigame_scene := preload("res://src/minigames/sword_bending_minigame.tscn")
+
+
 func _ready() -> void:
 	$AnimationPlayer.play("clash_swords")
 
@@ -8,7 +11,15 @@ func _ready() -> void:
 # Animation loop
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "clash_swords":
-		await get_tree().create_timer(1).timeout
-		$AnimationPlayer.play("clash_swords_player_win")
-	elif anim_name == "clash_swords_player_win":
+		var minigame: Minigame = minigame_scene.instantiate()
+		$CanvasLayer.add_child(minigame)
+		match await minigame.game_end:
+			Minigame.GameResult.VICTORY:
+				$AnimationPlayer.play("clash_swords_player_win")
+			Minigame.GameResult.DEFEAT:
+				$AnimationPlayer.play("clash_swords_enemy_win")
+			Minigame.GameResult.DRAW:
+				$AnimationPlayer.play("clash_swords_retreat")
+		$CanvasLayer.remove_child(minigame)
+	else:
 		$AnimationPlayer.play("clash_swords")
--- src/minigames/sword_bending_minigame.gd
+++ src/minigames/sword_bending_minigame.gd
@@ -6,7 +6,7 @@ var sword_rotation := 0.0
 var rotation_clockwise := false
 
 
-enum GameResult {VICORY, DEFEAT, DRAW}
+enum GameResult {VICTORY, DEFEAT, DRAW}
 
 
 signal game_end(result: GameResult)
@@ -19,4 +19,31 @@ func _process(delta: float) -> void:
 		sword_rotation -= delta
 	if rotation_clockwise and sword_rotation >= 0.3 or (not rotation_clockwise) and sword_rotation <= -0.3:
 		rotation_clockwise = not rotation_clockwise
-	($TextureRect.material as ShaderMaterial).set_shader_parameter("rotation", sword_rotation)
+	($Player.material as ShaderMaterial).set_shader_parameter("rotation", sword_rotation)
+
+
+func end_game() -> void:
+	set_process(false)
+	randomize()
+	if randi_range(0, 1):
+		$Enemy.position = Vector2i(576, 172)
+		$Enemy.flip_v = true
+	else:
+		$Enemy.position = Vector2i(552, 204)
+		$Enemy.flip_v = false
+	$Enemy.visible = true
+	await get_tree().create_timer(2).timeout
+	if $Enemy.flip_v == ((sword_rotation < 0) == rotation_clockwise):
+		game_end.emit(GameResult.VICTORY)
+	else:
+		game_end.emit(GameResult.DEFEAT)
+
+
+func _on_timer_timeout() -> void:
+	game_end.emit(GameResult.DRAW)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		remove_child($Timer)
+		end_game()
